fb4b4840a4d17078b55ae48547326707046818f4
cvetlo <paulthh@mail.ru>
2023-02-22 10:18:39 +0300

    create Lesson_4

--- task_6.py
+++ task_6.py
@@ -1,7 +1,18 @@
-''' Задание 6 Реализовать функцию int_func(),
-принимающую слова из маленьких латинских букв и возвращающую их же,
-но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.'''
+''' Задание 6 Реализовать два небольших скрипта:'''
+#итератор, генерирующий целые числа, начиная с указанного;
+def function():
+    a = int(input('Введите стартовое число: '))
+    from itertools import islice
+    from itertools import count
+    for i in islice(count(a), 20):
+        print(i)
+function()
 
-def int_func(text):
-    return text.title()
-print(int_func(input("Введите слово: ")))
+#итератор, повторяющий элементы некоторого списка, определённого заранее.
+def function_2():
+    list = [1, 2, "number", 3]
+    from itertools import islice
+    from itertools import cycle
+    for el in islice(cycle(list), 100):
+        print(el)
+function_2()
